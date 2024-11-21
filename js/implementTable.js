26c379a9e1f935b0b13024f28d6a2fbbe4e08546
Raam Udayappan <raamudayappan@gmail.com>
2024-11-21 15:39:52 -0500

    Updated Cuisine type to contemporary and converted to USD

--- js/implementTable.js
+++ js/implementTable.js
@@ -1,9 +1,9 @@
 var dataSet = [
-    ["Rose's Luxury", 'American/Variety', 'Washington DC', '90-120$', '10', 'Incredible tasting menu. One Michilin star'],
+    ["Rose's Luxury", 'Contemporary', 'Washington DC', '90-120$', '10', 'Incredible tasting menu. One Michilin star'],
     ["Barcelona Wine Bar", 'Tapas', 'Washington DC', '45-50$', '10', 'Really good, but really busy. Parking is also a pain'],
     ["Chima", 'Brazilian', 'Tysons, Virginia', '70$', '7', 'The Cinnamon Pineapple is fire, others like the lamb chops'],
-    ["Jikasei Mensho", 'Japanese', 'Tokyo, Japan', '1000¥', '8', 'Best Shio Ramen I had in Japan. Inside a fun area too'],
-    ["Canlis", 'American/Variety', 'Seattle, Washington', '180$', '6', 'Food was not up to par with the price. Venue was incredible'],
+    ["Jikasei Mensho", 'Japanese', 'Tokyo, Japan', '7-10$', '8', 'Best Shio Ramen I had in Japan. Inside a fun area too'],
+    ["Canlis", 'Contemporary', 'Seattle, Washington', '180$', '6', 'Food was not up to par with the price. Venue was incredible'],
     ["Ramen Danbo", 'Japanese', 'Seattle, Washington', '20$', '7', 'Best Ramen in Seattle, but a low bar. Get the spicy classic'],
     ["Neighborhood Ramen", 'Japanese', 'Philidelphia, Pennsylvania', '20$', '5', 'Considered the best by a connoisseur in Philadelphia, not for me'],
     ["Hajimaru", 'Japanese', 'Philidelphia, Pennsylvania', '14$', '7', 'My go to Ramen spot in Fishtown, get the signature'],
